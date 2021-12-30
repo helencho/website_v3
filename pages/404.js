690d3f1edad3df3c89e024bfebc546f76cd91d15
helencho <helencho@ac.c4q.nyc>
2021-12-30 12:34:27 -0500

    add 404 page

---
+++ pages/404.js
@@ -0,0 +1,12 @@
+import Head from 'next/head';
+
+export default function Custom404() {
+  return (
+    <>
+      <Head>
+        <title>Page Not Found | 404</title>
+      </Head>
+      <h1>Are you lost?</h1>
+    </>
+  );
+}
