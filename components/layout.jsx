d1b607b2ec6f62c86ae5d8d2b4336768438afc28
helencho <helencho@ac.c4q.nyc>
2021-12-31 16:26:18 -0500

    initial page layout

--- components/layout.jsx
+++ components/layout.jsx
@@ -1,10 +1,12 @@
 import Nav from './nav';
 
+import styles from './layout.module.scss';
+
 export default function Layout({ children }) {
   return (
     <>
       <Nav />
-      <main>{children}</main>
+      <main className={styles.main}>{children}</main>
     </>
   );
 }
