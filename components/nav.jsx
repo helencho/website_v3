591cffd3c43dca6f9eca8915d0eb8b77988fede1
helencho <helencho@ac.c4q.nyc>
2021-12-31 15:38:14 -0500

    add active states on the nav and update hover/focus states

--- components/nav.jsx
+++ components/nav.jsx
@@ -1,10 +1,15 @@
 import classNames from 'classnames';
 
 import Link from 'next/link';
+import { useRouter } from 'next/router';
 
 import styles from './nav.module.scss';
 
 const ITEMS = [
+  {
+    href: '/',
+    text: 'Home',
+  },
   {
     href: '/about',
     text: 'About',
@@ -20,19 +25,25 @@ const ITEMS = [
 ];
 
 const Nav = () => {
+  // TODO: When using mouseclick, do not apply focus colors
+
+  const router = useRouter();
+
   return (
     <nav className={styles.nav}>
-      <Link href="/">
-        <a className={classNames(styles.link, styles.link_home)}>Home</a>
-      </Link>
       <ul className={styles.list}>
-        {ITEMS.map((item) => (
-          <li className={styles.item} key={item.text}>
-            <Link href={item.href}>
-              <a className={styles.link}>{item.text}</a>
-            </Link>
-          </li>
-        ))}
+        {ITEMS.map((item) => {
+          const linkClasses = classNames(styles.link, {
+            [styles.link_active]: router.asPath === item.href,
+          });
+          return (
+            <li className={styles.item} key={item.text}>
+              <Link href={item.href}>
+                <a className={linkClasses}>{item.text}</a>
+              </Link>
+            </li>
+          );
+        })}
       </ul>
     </nav>
   );
