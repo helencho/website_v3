38f7f10ccb326d68a83d263a7f45e26c4c6b0b6b
helencho <helencho@ac.c4q.nyc>
2021-12-31 22:14:22 -0500

    add spacing variables, initial home page content

--- pages/index.js
+++ pages/index.js
@@ -1,17 +1,18 @@
 import Head from 'next/head';
 
+import styles from './index.module.scss';
+
 function Home() {
   return (
     <>
       <Head>
         <title>Helen Cho</title>
-        <meta
-          name="description"
-          content="About Helen Cho, a software engineer."
-        />
+        <meta name="description" content="Helen Cho, a software engineer." />
         <meta name="viewport" content="initial-scale=1.0, width=device-width" />
       </Head>
-      <div>Welcome</div>
+      <h1 className={styles.name}>
+        <span>Hae Young</span> <span>Helen</span> <span>Cho</span>
+      </h1>
     </>
   );
 }
